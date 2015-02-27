532c2736ef15003ec7f22d4c4814bc5307fb84ea
BigEgg <cd_bigegg@hotmail.com>
2015-02-27 19:29:52 +0800

    Add member service layer business logics.

---
+++ WeChat-IO/src/main/java/com/thoughtworks/wechat_io/services/MemberService.java
@@ -0,0 +1,73 @@
+package com.thoughtworks.wechat_io.services;
+
+import com.thoughtworks.wechat_io.core.Label;
+import com.thoughtworks.wechat_io.core.Member;
+import com.thoughtworks.wechat_io.jdbi.MemberDAO;
+import org.joda.time.DateTime;
+
+import java.util.Optional;
+
+import static com.thoughtworks.wechat_core.util.DateTimeExtension.toUnixTimestamp;
+import static com.thoughtworks.wechat_core.util.precondition.ArgumentPrecondition.checkNotBlank;
+import static org.apache.commons.lang.Validate.notNull;
+
+public class MemberService {
+    private MemberDAO memberDAO;
+    private LabelService labelService;
+
+    public MemberService(MemberDAO memberDAO, LabelService labelService) {
+        this.memberDAO = memberDAO;
+        this.labelService = labelService;
+    }
+
+    public Optional<Member> getMemberByOpenId(String openId) {
+        checkNotBlank(openId);
+
+        Member member = memberDAO.getMemberByOpenId(openId);
+        return Optional.ofNullable(member);
+    }
+
+    public Member subscribeMember(String openId) {
+        checkNotBlank(openId);
+
+        Member member = memberDAO.getMemberByOpenId(openId);
+        if (member != null) {
+            if (!member.isSubscribed()) {
+                memberDAO.updateSubscribed(member.getId(), true);
+            }
+        } else {
+            memberDAO.createMember(openId, toUnixTimestamp(DateTime.now()));
+        }
+        return memberDAO.getMemberByOpenId(openId);
+    }
+
+    public void unsubscribeMember(String openId) {
+        checkNotBlank(openId);
+
+        Member member = memberDAO.getMemberByOpenId(openId);
+        if (member != null && member.isSubscribed()) {
+            memberDAO.updateSubscribed(member.getId(), false);
+        }
+    }
+
+    public void linkMemberToLabel(Member member, Label label) {
+        notNull(member);
+        notNull(label);
+
+        Optional<Label> currentLabel = labelService.getMemberLabels(member);
+        if (currentLabel.isPresent()) {
+            memberDAO.updateMemberLabel(member.getId(), label.getId());
+        } else {
+            memberDAO.linkMemberWithLabel(member.getId(), label.getId());
+        }
+    }
+
+    public void delinkMemberLabel(Member member) {
+        notNull(member);
+
+        Optional<Label> currentLabel = labelService.getMemberLabels(member);
+        if (currentLabel.isPresent()) {
+            memberDAO.delinkMemberWithLabel(member.getId(), currentLabel.get().getId());
+        }
+    }
+}
---
+++ WeChat-IO/src/test/java/com/thoughtworks/wechat_io/services/MemberServiceTest.java
@@ -0,0 +1,171 @@
+package com.thoughtworks.wechat_io.services;
+
+import com.thoughtworks.wechat_io.core.Label;
+import com.thoughtworks.wechat_io.core.Member;
+import com.thoughtworks.wechat_io.jdbi.MemberDAO;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.sql.Timestamp;
+import java.util.Optional;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MemberServiceTest {
+    @Mock
+    private MemberDAO memberDAO;
+    @Mock
+    private LabelService labelService;
+    private MemberService memberService;
+
+    @Before
+    public void setUp() throws Exception {
+        memberService = new MemberService(memberDAO, labelService);
+    }
+
+    @Test
+    public void testGetMemberByOpenId_Exist() throws Exception {
+        when(memberDAO.getMemberByOpenId("openId")).thenReturn(createMember());
+
+        Optional<Member> member = memberService.getMemberByOpenId("openId");
+
+        verify(memberDAO, times(1)).getMemberByOpenId(eq("openId"));
+        assertThat(member.isPresent(), equalTo(true));
+        assertThat(member.get().getId(), equalTo(1L));
+        assertThat(member.get().getWeChatOpenId(), equalTo("openId"));
+        assertThat(member.get().isSubscribed(), equalTo(false));
+    }
+
+    @Test
+    public void testGetMemberByOpenId_NotExist() throws Exception {
+        when(memberDAO.getMemberByOpenId("openId")).thenReturn(null);
+
+        Optional<Member> member = memberService.getMemberByOpenId("openId");
+
+        verify(memberDAO, times(1)).getMemberByOpenId(eq("openId"));
+        assertThat(member.isPresent(), equalTo(false));
+    }
+
+    @Test
+    public void testSubscribeMember_NotExist() throws Exception {
+        Member mockMember = createMember();
+        when(memberDAO.getMemberByOpenId("openId")).thenReturn(null, mockMember);
+        when(memberDAO.createMember(eq("openId"), any(Timestamp.class))).thenReturn(mockMember.getId());
+
+        Member member = memberService.subscribeMember("openId");
+
+        verify(memberDAO, times(2)).getMemberByOpenId(eq("openId"));
+        verify(memberDAO, times(1)).createMember(eq("openId"), any(Timestamp.class));
+        verify(memberDAO, never()).updateSubscribed(anyLong(), anyBoolean());
+        assertThat(member, notNullValue());
+        assertThat(member.getId(), equalTo(1L));
+        assertThat(member.getWeChatOpenId(), equalTo("openId"));
+        assertThat(member.isSubscribed(), equalTo(false));
+    }
+
+    @Test
+    public void testSubscribeMember_Exist() throws Exception {
+        when(memberDAO.getMemberByOpenId("openId")).thenReturn(createMember(), createSubscribeMember());
+
+        Member member = memberService.subscribeMember("openId");
+
+        verify(memberDAO, times(2)).getMemberByOpenId(eq("openId"));
+        verify(memberDAO, times(1)).updateSubscribed(eq(1L), eq(true));
+        verify(memberDAO, never()).createMember(anyString(), any(Timestamp.class));
+        assertThat(member, notNullValue());
+        assertThat(member.getId(), equalTo(1L));
+        assertThat(member.getWeChatOpenId(), equalTo("openId"));
+        assertThat(member.isSubscribed(), equalTo(true));
+    }
+
+    @Test
+    public void testUnsubscribeMember_Exist() throws Exception {
+        when(memberDAO.getMemberByOpenId("openId")).thenReturn(createSubscribeMember());
+
+        memberService.unsubscribeMember("openId");
+
+        verify(memberDAO, times(1)).getMemberByOpenId(eq("openId"));
+        verify(memberDAO, times(1)).updateSubscribed(eq(1L), eq(false));
+    }
+
+    @Test
+    public void testUnsubscribeMember_NotExist() throws Exception {
+        when(memberDAO.getMemberByOpenId("openId")).thenReturn(null);
+
+        memberService.unsubscribeMember("openId");
+
+        verify(memberDAO, times(1)).getMemberByOpenId(eq("openId"));
+        verify(memberDAO, never()).updateSubscribed(anyLong(), anyBoolean());
+    }
+
+    @Test
+    public void testLinkMemberToLabel_NotLinked() throws Exception {
+        Member member = createSubscribeMember();
+        Label label = createLabel1();
+        when(labelService.getMemberLabels(member)).thenReturn(Optional.empty());
+
+        memberService.linkMemberToLabel(member, label);
+
+        verify(labelService, times(1)).getMemberLabels(eq(member));
+        verify(memberDAO, times(1)).linkMemberWithLabel(eq(member.getId()), eq(label.getId()));
+        verify(memberDAO, never()).updateMemberLabel(anyLong(), anyLong());
+    }
+
+    @Test
+    public void testLinkMemberToLabel_Linked() throws Exception {
+        Member member = createSubscribeMember();
+        when(labelService.getMemberLabels(member)).thenReturn(Optional.of(createLabel1()));
+
+        memberService.linkMemberToLabel(member, createLabel2());
+
+        verify(labelService, times(1)).getMemberLabels(eq(member));
+        verify(memberDAO, times(1)).updateMemberLabel(eq(member.getId()), eq(2L));
+        verify(memberDAO, never()).linkMemberWithLabel(anyLong(), anyLong());
+    }
+
+    @Test
+    public void testDelinkMemberLabel_Linked() throws Exception {
+        Member member = createSubscribeMember();
+        when(labelService.getMemberLabels(member)).thenReturn(Optional.of(createLabel1()));
+
+        memberService.delinkMemberLabel(member);
+
+        verify(labelService, times(1)).getMemberLabels(eq(member));
+        verify(memberDAO, times(1)).delinkMemberWithLabel(eq(member.getId()), eq(1L));
+    }
+
+    @Test
+    public void testDelinkMemberLabel_NotLinked() throws Exception {
+        Member member = createSubscribeMember();
+        when(labelService.getMemberLabels(member)).thenReturn(Optional.empty());
+
+        memberService.delinkMemberLabel(member);
+
+        verify(labelService, times(1)).getMemberLabels(eq(member));
+        verify(memberDAO, never()).delinkMemberWithLabel(anyLong(), anyLong());
+    }
+
+    private Member createMember() {
+        return new Member(1L, "openId", false);
+    }
+
+    private Member createSubscribeMember() {
+        return new Member(1L, "openId", true);
+    }
+
+    private Label createLabel1() {
+        return new Label(1, "Label1");
+    }
+
+    private Label createLabel2() {
+        return new Label(2, "Label2");
+    }
+}
