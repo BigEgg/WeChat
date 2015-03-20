6dc3097c3b3829402d78b769dc223512bc8f0067
BigEgg <cd_bigegg@hotmail.com>
2015-03-21 02:07:52 +0800

    Update the application's configuration.

--- WeChat-Application/src/main/java/com/thoughtworks/wechat_application/WeChatApplicationConfiguration.java
+++ WeChat-Application/src/main/java/com/thoughtworks/wechat_application/WeChatApplicationConfiguration.java
@@ -1,15 +1,28 @@
 package com.thoughtworks.wechat_application;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.thoughtworks.wechat_application.configs.CacheConfiguration;
 import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 public class WeChatApplicationConfiguration extends Configuration {
     @NotNull
     private CacheConfiguration cacheConfiguration;
 
+    @Valid
+    @NotNull
+    private DataSourceFactory database = new DataSourceFactory();
+
     public CacheConfiguration getCacheConfiguration() {
         return cacheConfiguration;
     }
+
+    @JsonProperty("database")
+    public DataSourceFactory getDataSourceFactory() {
+        return database;
+    }
+
 }
