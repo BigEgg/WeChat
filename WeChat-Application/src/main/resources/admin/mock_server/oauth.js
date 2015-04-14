5ce717b85debcdfff882716b4da37ab8098e4f89
BigEgg <cd_bigegg@hotmail.com>
2015-04-14 20:20:41 +0800

    Add refresh API for mock server

--- WeChat-Application/src/main/resources/admin/mock_server/oauth.js
+++ WeChat-Application/src/main/resources/admin/mock_server/oauth.js
@@ -1,8 +1,19 @@
 exports.accessToken = function (req, res) {
-    if (req.body.clientId === "BigEgg") {
+    if (req.body.clientId === 'BigEgg') {
         res.send({
-            access_token: "access",
-            refresh_token: "refresh"
+            access_token: 'access',
+            refresh_token: 'refresh'
+        });
+    } else {
+        res.sendStatus(401);
+    }
+};
+
+exports.refresh = function (req, res) {
+    if (req.body.access_token === 'access' && req.body.refresh_token === 'refresh') {
+        res.send({
+            access_token: 'access',
+            refresh_token: 'refresh'
         });
     } else {
         res.sendStatus(401);
