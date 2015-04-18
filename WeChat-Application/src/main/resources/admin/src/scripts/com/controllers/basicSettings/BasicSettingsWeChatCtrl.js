14d5534eda69d68b810d4b72b72b5f603da508f5
BigEgg <cd_bigegg@hotmail.com>
2015-04-18 17:07:13 +0800

    Add the refresh server status function in front-end.

--- WeChat-Application/src/main/resources/admin/src/scripts/com/controllers/basicSettings/BasicSettingsWeChatCtrl.js
+++ WeChat-Application/src/main/resources/admin/src/scripts/com/controllers/basicSettings/BasicSettingsWeChatCtrl.js
@@ -6,30 +6,34 @@ admin.app.controller('BasicSettingsWeChatCtrl', ['$scope', 'notify', 'weChatBasi
 
     $scope.status = {};
     $scope.status.loading = true;
+    $scope.status.statusGetting = false;
 
-    $scope.refreshStatus = function () {
-
+    $scope.getServerStatus = function () {
+        $scope.status.statusGetting = true;
+        weChatBasicSettingsSrv.getWeChatServerStatus().then(
+            function (data) {
+                $scope.status.loading = false;
+                $scope.status.statusGetting = false;
+                $scope.weChatServerStatus.entryPoint = data.entry_point;
+                $scope.weChatServerStatus.appToken = data.token;
+                $scope.weChatServerStatus.connectionStatus = data.connection;
+            },
+            function (error) {
+                $scope.status.loading = false;
+                $scope.status.statusGetting = false;
+                if (error instanceof AuthenticateFailedException) {
+                    notify.danger(error.message);
+                }
+                else {
+                    notify.warning(error.message);
+                }
+            }
+        );
     };
 
     $scope.getNewToken = function () {
-
+        console.log('new token');
     };
 
-    weChatBasicSettingsSrv.getWeChatServerStatus().then(
-        function (data) {
-            $scope.status.loading = false;
-            $scope.weChatServerStatus.entryPoint = data.entry_point;
-            $scope.weChatServerStatus.appToken = data.token;
-            $scope.weChatServerStatus.connectionStatus = data.connection;
-        },
-        function (error) {
-            $scope.status.loading = false;
-            if (error instanceof AuthenticateFailedException) {
-                notify.danger(error.message);
-            }
-            else {
-                notify.warning(error.message);
-            }
-        }
-    );
+    $scope.getServerStatus();
 }]);
