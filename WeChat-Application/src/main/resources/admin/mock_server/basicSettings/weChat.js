0157ba34b42c265ed426fae907fc1da8bb173fd6
BigEgg <cd_bigegg@hotmail.com>
2015-04-18 16:23:56 +0800

    Bind the html to server status controller.

--- WeChat-Application/src/main/resources/admin/mock_server/basicSettings/weChat.js
+++ WeChat-Application/src/main/resources/admin/mock_server/basicSettings/weChat.js
@@ -1,5 +1,5 @@
 exports.entryPoint = function (req, res) {
-    if (req.param('access_token') === 'access') {
+    if (req.query.access_token === 'access') {
         res.send({
             entry_point: 'http://localhost:3000/wechat',
             token: 'ABCDE_TOKEN',
--- WeChat-Application/src/main/resources/admin/src/scripts/com/controllers/basicSettings/BasicSettingsWeChatCtrl.js
+++ WeChat-Application/src/main/resources/admin/src/scripts/com/controllers/basicSettings/BasicSettingsWeChatCtrl.js
@@ -1,3 +1,35 @@
-admin.app.controller('BasicSettingsWeChatCtrl', ['$scope', function ($scope) {
+admin.app.controller('BasicSettingsWeChatCtrl', ['$scope', 'notify', 'weChatBasicSettingsSrv', function ($scope, notify, weChatBasicSettingsSrv) {
+    $scope.weChatServerStatus = {};
+    $scope.weChatServerStatus.entryPoint = '';
+    $scope.weChatServerStatus.appToken = '';
+    $scope.weChatServerStatus.connectionStatus = false;
 
+    $scope.status = {};
+    $scope.status.loading = true;
+
+    $scope.refreshStatus = function () {
+
+    };
+
+    $scope.getNewToken = function () {
+
+    };
+
+    weChatBasicSettingsSrv.getWeChatServerStatus().then(
+        function (data) {
+            $scope.status.loading = false;
+            $scope.weChatServerStatus.entryPoint = data.entry_point;
+            $scope.weChatServerStatus.appToken = data.token;
+            $scope.weChatServerStatus.connectionStatus = data.connection;
+        },
+        function (error) {
+            $scope.status.loading = false;
+            if (error instanceof AuthenticateFailedException) {
+                notify.danger(error.message);
+            }
+            else {
+                notify.warning(error.message);
+            }
+        }
+    );
 }]);
